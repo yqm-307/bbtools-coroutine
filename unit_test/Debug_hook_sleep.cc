fb4882c34b413bef7e8c4d3fa6e474ddcb4530a6
= <=>
2024-06-20 01:49:42 +0800

    debug文件添加注释

--- unit_test/Debug_hook_sleep.cc
+++ unit_test/Debug_hook_sleep.cc
@@ -10,12 +10,16 @@ int main()
     std::atomic_int exit_value = 0;
     g_scheduler->Start(true);
 
+    /* 两个协程注册时间相同。都执行休眠操作，唤醒事件相同，证明函数在sleep处无阻塞 */
+
+    /* 注册一个协程 */
     g_scheduler->RegistCoroutineTask([&](){
         ::sleep(1);
         printf("t1: %ld\n", bbt::clock::now<>().time_since_epoch().count());
         exit_value++;
     });
 
+    /* 注册一个协程 */
     g_scheduler->RegistCoroutineTask([&](){
         ::sleep(1);
         printf("t2: %ld\n", bbt::clock::now<>().time_since_epoch().count());
