5119aeeef67a378a58c2c116dd729b013f047ae8
yqm-307 <979336542@qq.com>
2024-07-12 09:56:43 +0000

    优化Chan使用方式，添加左移、右移运算符重载 Fixes #37

--- bbt/coroutine/coroutine.hpp
+++ bbt/coroutine/coroutine.hpp
@@ -16,4 +16,8 @@ namespace bbt::coroutine
 /* 获取当前运行的协程id，0为main线程中获取的id */
 detail::CoroutineId GetLocalCoroutineId();
 
+template<class TItem>
+inline typename sync::Chan<TItem>::SPtr MakeChan(size_t max = 65535)
+{ return std::make_shared<bbt::coroutine::sync::Chan<TItem>>(max); }
+
 }
