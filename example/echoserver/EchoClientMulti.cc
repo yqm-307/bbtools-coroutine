3957cd97bbe9c3c355aae794c18b10da6c62e2ec
= <=>
2024-08-18 23:19:57 +0800

    debug例程临时提交

--- example/echoserver/EchoClientMulti.cc
+++ example/echoserver/EchoClientMulti.cc
@@ -11,21 +11,23 @@ const char msg[] = "1";
 class Client
 {
 public:
-    Client(std::string ip, short port):
-        m_ip(ip), m_port(port), m_cdl(1){}
+    Client(std::string ip, short port, int round_num):
+        m_ip(ip), m_port(port), m_cdl(round_num), m_round_num(round_num){}
     ~Client(){}
 
     void Start() {
-        bbtco [this](){
-            _Run();
-            printf("success\n");
-            m_cdl.Down();
-        };
+        for (int i = 0; i < m_round_num; ++i) {
+            bbtco [this, i](){
+                _Run(i);
+                printf("end do once: %d\n", i);
+                m_cdl.Down();
+            };
+        }
         m_cdl.Wait();
     }
 
 protected:
-    void _Run() {
+    void _Run(int i) {
         sockaddr_in addr;
         addr.sin_addr.s_addr = inet_addr("127.0.0.1");
         addr.sin_port = htons(10010);
@@ -37,20 +39,17 @@ protected:
         Assert(fd >= 0);
         int ret = ::connect(fd, (sockaddr *)(&addr), sizeof(addr));
         Assert(ret == 0);
-        for (int i = 0; i<10000; ++i) {
-            ret = ::write(fd, msg, sizeof(msg));
-            Assert(ret != -1);
+        ret = ::write(fd, std::to_string(i).c_str(), 1);
+        Assert(ret != -1);
 
-            ret = ::read(fd, read_buf, sizeof(read_buf));
-            Assert(ret != -1);
-            Assert(ret == sizeof(msg));
-        }
-
-        ::sleep(1);
+        ret = ::read(fd, read_buf, sizeof(read_buf));
+        Assert(ret != -1);
+        Assert(ret == 1);
         ::close(fd);
     }
 private:
     bbt::thread::CountDownLatch m_cdl{1};
+    int m_round_num;
     std::string m_ip;
     short       m_port;
 };
@@ -58,7 +57,7 @@ private:
 int main()
 {
     g_scheduler->Start(true);
-    Client c{"127.0.0.1", 10010};
+    Client c{"127.0.0.1", 10010, 1000};
     c.Start();
 
     g_scheduler->Stop();
--- example/echoserver/EchoServer.cc
+++ example/echoserver/EchoServer.cc
@@ -13,43 +13,52 @@ public:
     ~Server(){}
 
     void Start(){
-        bbtco [this](){
+        // bbtco [this](){
             _Run();
             m_cdl.Down();
-        };
+        // };
 
         m_cdl.Wait();
     }
 
 protected:
     void _Run(){
-        int listen_fd = bbt::net::Util::CreateListen("", m_listen_port, true);
+        int listen_fd = bbt::net::Util::CreateListen("", m_listen_port, false);
+        // int listen_fd = bbt::net::Util::CreateListen("", m_listen_port, true);
         Assert(listen_fd >= 0);
 
         sockaddr_in cli_addr;
-        char *buf = new char[1024];
         socklen_t len = sizeof(cli_addr);
 
         while (1)
         {
+            char buf[2];
             int new_fd = ::accept(listen_fd, (sockaddr *)(&cli_addr), &len);
+            // bbtco [new_fd](){
             Assert(new_fd >= 0);
-            memset(buf, '\0', 1024);
 
-            Assert(bbt::net::Util::SetFdNoBlock(new_fd) == 0);
+            // Assert(bbt::net::Util::SetFdNoBlock(new_fd) == 0);
 
-            printf("[server] read msg! fd=%d\n", new_fd);
+            bool noclose = true;
+            while (noclose) {
+                int read_len = ::read(new_fd, buf, 2);
 
-            int read_len = ::read(new_fd, buf, 1024);
-            Assert(read_len > 0);
-            printf("[server] read msg succ! fd=%d msg=%s\n", new_fd, buf);
-            int write_len = ::write(new_fd, buf, read_len);
-            Assert(read_len == write_len);
+                if (read_len == 0)
+                    noclose = false;
+                
+                int write_len = ::write(new_fd, buf, read_len);
+                printf("echo: %s\n", buf);
+                Assert(read_len == write_len);
+
+
+            }
+
             ::close(new_fd);
+            printf("echo done!\n");
+            // };
+            fflush(stdout);
         }
-
         ::close(listen_fd);
-        delete[] buf;
     }
 private:
     bbt::thread::CountDownLatch m_cdl{1};
