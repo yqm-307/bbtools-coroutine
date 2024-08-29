c77044757a990549b43244c78d800940d3a0c38a
yqm-307 <979336542@qq.com>
2024-08-29 08:03:54 +0000

    DebugMgr添加CoEvent监视 Fixes #84

--- bbt/coroutine/detail/CoPollEvent.hpp
+++ bbt/coroutine/detail/CoPollEvent.hpp
@@ -10,12 +10,11 @@ namespace bbt::coroutine::detail
 
 
 /**
- * @brief 协程轮询事件
+ * @brief 协程轮询事件（无锁）
  * 
  * 辅助协程实现挂起和唤醒
  */
 class CoPollEvent:
-    // public IPollEvent,
     public std::enable_shared_from_this<CoPollEvent>
 {
 public:
@@ -43,7 +42,7 @@ public:
     int                             UnRegist();
 
 protected:
-    int                             _RegistCustomEvent();
+    // int                             _RegistCustomEvent();
     int                             _RegistFdEvent();
     int                             _CannelAllFdEvent();
 
