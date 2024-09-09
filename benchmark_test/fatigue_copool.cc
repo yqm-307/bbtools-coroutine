f3b36981f79210fd035b60d800f2fae55dc399ea
= <=>
2024-09-09 22:33:56 +0800

    协程池压测 Fixes #107

---
+++ benchmark_test/fatigue_copool.cc
@@ -0,0 +1,24 @@
+#include <bbt/coroutine/coroutine.hpp>
+
+using namespace bbt::coroutine;
+
+int main()
+{
+    const int nsum_co = 1000000;
+
+    g_scheduler->Start();
+
+    pool::CoPool pool{3000};
+
+    while (true)
+    {   
+        for (int i = 0; i < nsum_co; ++i)
+            pool.Submit([&](){});
+        sleep(1);
+    }
+
+
+    pool.Release();
+    g_scheduler->Stop();
+
+}
