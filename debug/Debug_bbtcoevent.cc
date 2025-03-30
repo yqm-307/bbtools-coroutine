b415deb2881ef3e4c71326ac0f716b6fbec1a8d7
= <=>
2025-03-30 18:40:38 +0800

    EventHelper事件重复bug Fixes #149

--- debug/Debug_bbtcoevent.cc
+++ debug/Debug_bbtcoevent.cc
@@ -23,12 +23,53 @@ void debug_1()
     }
 }
 
+void debug_2()
+{
+    bbtco [](){
+        while (true)
+        {
+            printf("======== turn once =========\n");
+            // for (int i = 0; i < 1000; ++i)
+            {
+                auto cond = bbtco_make_cocond();
+                auto chan = bbt::co::sync::Chan<int, 10>();
+                std::atomic_int count{0};
+    
+                __bbtco_event_regist_ex(-1, bbtco_emev_persist, 10)
+                [&](int fd, short events) -> bool
+                {
+                    printf("enter once!\n", count.load());
+                    count++;
+                    if (count <= 5) {
+                        printf("write: %d\n", count.load());
+                        chan << count.load();
+                        return true;
+                    }
+        
+                    return false;
+                };
+        
+                for (int i = 0; i < 5; ++i)
+                {
+                    int val = 0;
+                    chan >> val;
+                    printf("read: %d\n", val);
+                }
+                chan.Close();
+                Assert(count == 5);
+            }
+        }
+    };
+    sleep(10000);
+}
+
 int main()
 {
     g_scheduler->Start();
     std::atomic_int val = 0;
 
-    debug_1();
+    // debug_1();
+    debug_2();
 
     g_scheduler->Stop();
 }
