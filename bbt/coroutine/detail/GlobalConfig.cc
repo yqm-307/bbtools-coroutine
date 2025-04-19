0e0f64a476c3928a68b470bfb5d5ee7322dfa629
= <=>
2025-04-19 13:06:13 +0800

    使用更健壮的写法

--- bbt/coroutine/detail/GlobalConfig.cc
+++ bbt/coroutine/detail/GlobalConfig.cc
@@ -9,7 +9,7 @@ namespace bbt::coroutine::detail
 GlobalConfig::GlobalConfig()
 {
     if (m_cfg_static_thread_num <= 0)
-        m_cfg_static_thread_num = std::thread::hardware_concurrency();
+        m_cfg_static_thread_num = std::thread::hardware_concurrency() > 0 ? std::thread::hardware_concurrency() : 1;
 }
 
 } // namespace bbt::coroutine::detail
